35453400ecc624630d56d5ca9f0c34912805cabc
Dener <ldenerjesus@gmail.com>
2023-07-15 07:57:28 -0300

    exercicio 2 media queries

---
+++ Módulo 4/exercicios/ex025-fomularios/func.js
@@ -0,0 +1,8 @@
+var atual = Number(new Date().getFullYear())
+
+function calcIdade(){
+    let atual = Number(new Date().getFullYear())
+    let nascimento = Number(iano.value)
+
+    iidade.innerHTML = atual - nascimento
+}
